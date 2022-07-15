6eb085a69d395e2a96021b9c74958a2f01f375e6
Turann <84424530+turannul@users.noreply.github.com>
2022-07-15 05:23:35 +0300

    Delete down.sh

--- files/down.sh
+++
@@ -1,4 +0,0 @@
-apt-get download com.randy420.tai --allow-unauthenticated ;
-apt-get download  com.randy420.taidependscoolstar --allow-unauthenticated ;
-apt-get download com.randy420.taidependsother --allow-unauthenticated ;
-apt-get download com.randy420.tools --allow-unauthenticated ;
